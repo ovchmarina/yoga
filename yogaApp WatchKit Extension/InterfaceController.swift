424f16480b153cb166dc193cf592c77e235bd434
“ovchmarina” <marinakabakova84@gmail.com>
2020-12-08 14:19:52 +0300

    Initial Commit

---
+++ yogaApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  yogaApp WatchKit Extension
+//
+//  Created by user on 08/12/2020.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
